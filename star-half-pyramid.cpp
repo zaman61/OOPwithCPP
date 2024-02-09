0232b9660696c10ee2f23f9bf89fc13d5e72afa8
zaman61 <156302838+zaman61@users.noreply.github.com>
2024-02-09 10:23:05 -0500

    Update

---
+++ star-half-pyramid.cpp
@@ -0,0 +1,25 @@
+//write a program to print half pyramid 
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter the number of rows: ";
+    cin >> n;
+
+    for(int i=1; i<=n; ++i)
+    {
+        for(int j=1; j<=i; ++j)
+        {
+
+            cout << "*";
+
+        }
+        cout<<endl;
+
+
+    }
+    return 0;
+}
